f3ea013b4e986c87f7d4b89cbe420337420f8963
robinristo78 <robinristo@gmail.com>
2024-11-12 23:38:15 +0200

    Mängu info + Play nupp

--- Projekt/js/scripts.js
+++ Projekt/js/scripts.js
@@ -5,3 +5,25 @@ function closePage() {
     // Varuvõimalusena suunake ümber tühjale lehele või about:blank
     window.location.href = 'about:blank';
 }
+
+function showInfo(name, link) {
+    const infoBox = document.getElementById('info-box');
+    const infoContent = document.getElementById('info-content');
+
+    infoContent.innerHTML = '';
+    
+    // Set the content inside the element
+    infoContent.innerHTML += `<h1 class="text-white" style="text-align: center;">${name}</h1> <br> <div style="width: 12rem;">`;
+
+    infoContent.innerHTML += `<p class="bg-light flex p-3 text-black">Desc</p>`
+    
+    infoContent.innerHTML += `<div class="m-2" style="justify-content: center; text-align: center;"><a href="${link}" class="bg-primary bold" style="padding-left: 1rem; padding-right: 1rem; font-size: 40px;">PLAY</a></div>`;
+
+    // Show the modal
+    infoBox.classList.remove('hidden');
+}
+
+function closeInfo() {
+    const infoBox = document.getElementById('info-box');
+    infoBox.classList.add('hidden');
+}
